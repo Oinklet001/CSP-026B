20e87be7eca5af9828d602b050e15303789b395c
Oinklet001 <trwer1422@gmail.com>
2017-08-21 23:33:51 -0700

    Update BankAccount.java

--- Programming Lab 1/BankAccount.java
+++ Programming Lab 1/BankAccount.java
@@ -1,62 +1,54 @@
-public class BankAccount {
+public abstract class BankAccount
+{
+   public double balance;
+   public double deposits;
+   public double withdraws;
+   public double annualIR;
+   public double monthlySC;
 
-    public double balance;
-    public double depositsAMonth;
-    public double withdrawsAMonth;
-    public double annualIR;
-    public double monthlyServiceC;
 
-  //A no arguemtn constructor
 
-  public BankAccount()
+   //This is the constructor
+   public double constructor(double balance1, double annualIR1) 
+   {
+     balance = balance1;
+     annualIR = annualIR1;
+     return balance;
+   }
 
-    {
+   public void deposit(double total)
+   {
+     balance = balance + total;
+     deposits++;
+    
+   }
+   public void withdraw (double total)
+   {
+     balance = balance - total;
+     withdraws++;
+   }
 
-    }   
-   
-    //constructor
-    public BankAccount(double balance, double annualIR){
+   private void calcInterest() 
+   {
+     double monthlyIR;
+     double monthlyI;
+     
+     monthlyIR = (annualIR / 12);
+     monthlyI = balance * monthlyIR;
+     balance = balance + monthlyI;
+   }
 
-    this.balance = balance;
-    this.annualIR = annualIR;
+   protected void monthlyProcess() 
+   {
+     balance = balance - monthlySC;
+     this.calcInterest();
+     deposits = 0;
+     withdraws = 0;
+     monthlySC = 0;
+   }
 
-    }
-    //number of deposites
-       public void deposit(double total)
-        {
-            balance += total;
-            depositsAMonth += total;
-        }
-        //number of withdrawls
-          public void withdraw(double total)
-        {
-            balance -= total;
-            withdrawsAMonth += total;
-        }
-        
-        public double calcInterest(double balance, double annualIR)
-        {
-        double monthlyIR = annualIR / 12;
-
-        monthlyIR = balance * monthlyIR;
-
-        balance = balance + monthlyIR;
-        
-        return balance;
-        }
-        
-        public double monthlyProcess(double balance, double depositsAMonth, double withdrawsAMonth)
-        {
-        balance = balance - monthlyServiceC;
-        withdrawsAMonth = 0;
-        depositsAMonth = 0;
-        monthlyServiceC = 0;
-        
-        return balance;
-        }
-
-		public void monthlyProcess() {
-			// TODO Auto-generated method stub
-			
-		}
+   public double getBalance()
+   {
+     return balance;
+   }
 }
