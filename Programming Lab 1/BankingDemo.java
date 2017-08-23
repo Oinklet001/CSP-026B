d63dde62ebbcf50765566f7940413dd1170b5003
Oinklet001 <trwer1422@gmail.com>
2017-08-22 18:11:57 -0700

    Update BankingDemo.java

--- Programming Lab 1/BankingDemo.java
+++ Programming Lab 1/BankingDemo.java
@@ -2,22 +2,29 @@ import java.util.Scanner;
 
 public class BankDemo {
 
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 
+		//user enter an input in whether they want to withdraw or not.
 		System.out.println("Press D to deposite or w to withdraw.");
 		char choice = scanner.nextLine().charAt(0);
 		
-		 if (choice == 'd' || choice == 'D')
+		//It grabs the user's input and determines what options they get.
+		if (choice == 'd' || choice == 'D')
          {
          
          }
 		 else if (choice == 'w' || choice == 'W')
 		 {
-			 System.out.println("What is your balance");
+			 System.out.println("What is your balance");//Asks user their current balance.
 			 double balance = scanner.nextDouble();
-			 SavingsAccount saveA = new SavingsAccount(balance); 
+			 
+			 SavingsAccount saveA = new SavingsAccount(balance); //communicates to the class SavingsAccount.
+			 
+			 saveA.withdraw(balance);
 			 
 			 System.out.println(saveA);
 			 
