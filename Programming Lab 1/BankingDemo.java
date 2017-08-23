98c162408f1c5dc75ee81b631230d63f16d9949b
Oinklet001 <trwer1422@gmail.com>
2017-08-22 23:01:32 -0700

    Update BankingDemo.java

--- Programming Lab 1/BankingDemo.java
+++ Programming Lab 1/BankingDemo.java
@@ -6,7 +6,12 @@ public class BankDemo {
 
 	public static void main(String[] args) {
 		
+		//Saving account object
+		SavingAccount saveA = new SavingAccount();
+
+		//Scanner class
 		scanner = new Scanner(System.in);
+		
 
 		//user enter an input in whether they want to withdraw or not.
 		System.out.println("Press D to deposite or w to withdraw.");
@@ -15,16 +20,33 @@ public class BankDemo {
 		//It grabs the user's input and determines what options they get.
 		if (choice == 'd' || choice == 'D')
          {
-         
+			 System.out.println("What is your balance");//Asks user their current balance.
+			 double balance = scanner.nextDouble();
+			 
+			 System.out.println("how much money are you withdrawing?");//Asks user how much they are withdrawing.
+			 double amount = scanner.nextDouble();
+			 
+			 saveA.setBalance(balance);
+			  
+			 saveA.deposit(balance);
+			 
+			 System.out.println(saveA);
          }
 		 else if (choice == 'w' || choice == 'W')
 		 {
 			 System.out.println("What is your balance");//Asks user their current balance.
 			 double balance = scanner.nextDouble();
 			 
-			 SavingsAccount saveA = new SavingsAccount(balance); //communicates to the class SavingsAccount.
+			 System.out.println("how much money are you withdrawing?");//Asks user how much they are withdrawing.
+			 double amount = scanner.nextDouble();
 			 
+			 saveA.setBalance(balance);
+			  
 			 saveA.withdraw(balance);
+			 
+			// saveA.setMoney(amount);
+			  
+			// saveA.setMoney(amount);
 			 
 			 System.out.println(saveA);
 			 
