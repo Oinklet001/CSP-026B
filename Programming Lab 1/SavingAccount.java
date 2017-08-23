37391c28cbdd02b0d13f175f1426fcb6046cbe54
Oinklet001 <trwer1422@gmail.com>
2017-08-22 23:00:38 -0700

    Update SavingAccount.java

--- Programming Lab 1/SavingAccount.java
+++ Programming Lab 1/SavingAccount.java
@@ -1,18 +1,38 @@
-public class SavingsAccount extends BankAccount
+public class SavingAccount extends BankAccount
 {
+	private double total;
+	private double money;
 
-	//no argument constructor
-	public SavingsAccount(double balance)
+	//Sets the balance
+	public void setBalance(double total)
 	{
-
-
+		balance = total;
 	}
 	
-public void withdraw(double total) 
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	//Determines how much they want to use for depositing or withdrawing
+/**	public void setMoney(double money)
+	{
+		withdraws = money;
+	}
+	
+	public double getMoney()
+	{
+		return withdraws;
+	}**/
+	
+	
+public void withdraw(double balance) 
 {
   if(balance >= 25)
   {
     super.withdraw(total);
+    
+   // balance = balance - withdraws;
   
   System.out.println("You withdrew money." + balance );
   }
@@ -23,18 +43,27 @@ public void withdraw(double total)
   
 }
 
-public void deposit(double total) 
+public void deposit(double balance) 
 {
  if(balance >= 25)
-   super.deposit(total);
+ {
+	 super.deposit(total);
+	 
+    System.out.println("You deposited money." + balance );
+ }
+   else
+   {
+	  System.out.println("You are to low.");
+   }
 }
+
 
 protected void monthlyProcess()
 {
   if(withdraws > 4)
-	  monthlySC++;
+	  monthlySC = monthlySC + monthlySC;
    else
 	   System.out.println("You did not go over 4 withdrawls this month." );
 
-}
+   }
 }
